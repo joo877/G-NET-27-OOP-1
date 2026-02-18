aa3a31d0bda94300f131150670c38207203dff99
Youssef said <jooasaid2004@gmail.com>
2026-02-18 16:06:22 +0400

    Q01

--- OOP01/Program.cs
+++ OOP01/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
 
+
+            #region part01
+            #region Q1
+            /*
+                 calss                                           VS                            struct
+            1. reference type                                                                value type
+            2. supports inheritance                                                  does not support inheritance
+            3.using to represent complex data types                                  using to represent simple data types
+            4. access modifire with calss members ,                                   access modifire with struct members 
+            (public, private, protected, internal,                                      (public, private, internal.)
+            protected internal, private protected.)
+
+             */
+            #endregion
+            #endregion
         }
     }
 }
